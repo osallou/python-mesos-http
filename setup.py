6deb3da1f86fd108fe726e9d8e5c1ff166cce21e
Olivier Sallou <olivier.sallou@gmail.com>
2018-02-21 08:58:07 +0100

    prepare next release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ config = {
     'author': 'Olivier Sallou',
     'author_email': 'olivier.sallou@irisa.fr',
     'download_url': 'https://github.com/osallou/python-mesos-http',
-    'version': '0.2.11',
+    'version': '0.2.12',
      'classifiers': [
         # How mature is this project? Common values are
         #   3 - Alpha
